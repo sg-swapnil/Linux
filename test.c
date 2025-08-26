fd0fab5077b2db45ca2a0666206d1a27d62546aa
swapnil gawai <swapnilgawai326@gmail.com>
2025-08-26 16:35:40 +0530

    added power of 4 code here

--- test.c
+++ test.c
@@ -1,8 +1,32 @@
 #include<stdio.h>
-int b;
+// power of 2 and 4 examples
 int main()
 {
-	const int a;
-	int *ptr;
-
+	int var;// 0 1 0 0  // 16   0 0 0 1 0 0 0 0
+	int cnt=0,i,j=31;
+	printf("enter data:\n");
+	scanf("%d",&var);
+	while(var)
+	{
+		if((var>>j &1)==1)// checking the bit position which is set from left side
+		{
+			break;
+		}
+		else
+			j--;
+	}	 
+	for(i=j;i>=0;i--)// assigining that value to index i
+	{
+		if(((var>>i)&1)==0)
+		{
+			cnt++;
+		}
+	}
+	printf("%d\n",cnt);
+	if((cnt%2==0) && ((var & var-1)==0))
+	{
+		printf("power of 4\n");
+	}
+	else
+		printf("not\n");
 }
