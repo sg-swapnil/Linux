fdd169fe41d4727533ad9513b1445b0260cca18f
swapnil gawai <swapnilgawai326@gmail.com>
2025-09-05 15:49:48 +0530

    code clean up in mutex source file

--- mutex.c
+++ mutex.c
@@ -5,14 +5,16 @@
 //creating multiple threads checking the result by sharing global variable to each thread, then applying mutex for proper result
 int rel=0;
 pthread_mutex_t lock;
+pthread_t t[4];
+int id[4];
 void* thread_fun(void* arg)
 {
         pthread_mutex_lock(&lock);
-        int num= *(int*)arg;
+        int id= *(int*)arg;
+	printf("thread %d is in critical section:\n",id);
         for(int i=0;i<100;i++)
         {
                 rel+=i;
-        	printf("in thread_fun num:%d rel:%d\n",num,rel);
 		sleep(0.001);// if we did't add sleep it might show correct result without adding mutex its base on system scheduling..
         }
         pthread_mutex_unlock(&lock);
@@ -21,21 +23,16 @@ void* thread_fun(void* arg)
 }
 int main()
 {
-        pthread_t t[4];
         pthread_mutex_init (&lock,NULL);
-        int a=1,result=0,i;
+        int i;
         for(i=0;i<4;i++)
         {
-                pthread_create(&t[i], NULL,thread_fun, &a);
+ 		id[i]=i;
+		pthread_create(&t[i], NULL,thread_fun, &id[i]);
         }
         for(i=0;i<4;i++)
         {
                 pthread_join(t[i], NULL);
         }
-        for(i=0;i<4;i++)
-        {
-                result+=rel;
-        //        printf("in for loop rel=%d\n",rel);
-        }
-	printf("final:%d\n",result);
+	printf("result:%d\n",rel);
 }
